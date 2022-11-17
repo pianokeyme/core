9e97d930c14b8ac773c2d618d461a850b7cacc4b
noelkhalaf <noel_khalaf@hotmail.com>
2022-11-17 16:05:44 -0500

    Arduino: Setup arduino for wifi ssid and pass

--- arduino/AWS_IoT_WiFi/ap_initialization.ino
+++ arduino/AWS_IoT_WiFi/ap_initialization.ino
@@ -66,70 +66,48 @@ void listenForClients() {
     client = server.available();   // listen for incoming clients
 
     if (client) {                             // if you get a client,
-      Serial.println("new client");           // print a message out the serial port
+      Serial.print("new client");           // print a message out the serial port
       String currentLine = "";                // make a String to hold incoming data from the client
       while (client.connected()) {            // loop while the client's connected
         delayMicroseconds(10);                // This is required for the Arduino Nano RP2040 Connect - otherwise it will loop so fast that SPI will never be served.
         if (client.available()) {             // if there's bytes to read from the client,
           char c = client.read();             // read a byte, then
           Serial.write(c);                    // print it out the serial monitor
-          if (c == '\n') {                    // if the byte is a newline character
-
-            // if the current line is blank, you got two newline characters in a row.
-            // that's the end of the client HTTP request, so send a response:
-            if (currentLine.length() == 0) {
-              // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-              // and a content-type so the client knows what's coming, then a blank line:
-              client.println("HTTP/1.1 200 OK");
-              client.println("Content-type:text/html");
-              client.println();
-
-              // the content of the HTTP response follows the header:
-              // client.print("Click <a href=\"/H\">here</a> turn the LED on<br>");
-              // client.print("Click <a href=\"/L\">here</a> turn the LED off<br>");
-
-              client.print("<a href=\"/TEXT_SSID\">Submit SSID</a><br><br>"); // Replace TEXT with SSID to test
-              client.print("<a href=\"/TEXT_PASS\">Submit PASS</a><br><br>"); // REPLACE TEXT with PASS to test
-
-              // The HTTP response ends with another blank line:
-              client.println();
-              // break out of the while loop:
-              break;
+          // That's the end of the client HTTP request, so send a response:
+          if (c == '#') {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          }
+          else if (c == '\n') {      // if you got a newline, then clear currentLine:
+            if (currentLine.indexOf("wifi_ssid") >= 0) {
+              ssid = currentLine.substring(10);
             }
-            else {      // if you got a newline, then clear currentLine:
-              currentLine = "";
+            else if (currentLine.indexOf("wifi_pass") >= 0) {
+              pass = currentLine.substring(10);
             }
+            currentLine = "";
           }
           else if (c != '\r') {    // if you got anything else but a carriage return character,
             currentLine += c;      // add it to the end of the currentLine
-          }
-
-          // Check to see if the client request was "GET /H" or "GET /L":
-          if (currentLine.startsWith("GET /") && currentLine.endsWith("_SSID")) {
-            ssid = currentLine.substring(5, currentLine.length()-5);
-            Serial.println();
-            Serial.print("***Acquired Client SSID:: ");
-            Serial.println(ssid);
-            ssid_acquired = true;
-          }
-          if (currentLine.startsWith("GET /") && currentLine.endsWith("_PASS")) {
-            pass = currentLine.substring(5, currentLine.length()-5);
-            Serial.println();
-            Serial.print("***Acquired Client PASS:: ");
-            Serial.println(pass);
-            pass_acquired = true;
-          }
-          if (ssid_acquired && pass_acquired) {
-            Serial.println();
-            Serial.println("***** Both Client SSID and PASS Acquired *****");
-            Serial.println();
-            return;
           }
         }
       }
       // close the connection:
       client.stop();
+      Serial.println();
       Serial.println("client disconnected");
+      Serial.println();
+      Serial.print("***Acquired Client SSID:: ");
+      Serial.println(ssid);
+      Serial.print("***Acquired Client PASS:: ");
+      Serial.println(pass);
+      Serial.println();
+      return;
     }
   }
 }
